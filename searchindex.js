5da86e208e4a443f46b20d59199266149ab2ca30
qpc <king.r.paul@gmail.com>
2019-04-02 20:16:04 -0500

    re-organize

--- searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:55,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Dose Profile Utility\u2019s documentation!"],titleterms:{document:0,dose:0,indic:0,profil:0,tabl:0,util:0,welcom:0}})
